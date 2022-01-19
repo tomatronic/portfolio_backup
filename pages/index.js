f7401817d5be1b911884f5237ab8bcb1ca1fe7cf
tomatronic <tom.spencer@rakuten.com>
2022-01-19 20:55:46 +0000

    Added gtag back in

--- pages/index.js
+++ pages/index.js
@@ -58,32 +58,32 @@ function HomePage() {
       </div>
       <a name="work" id="work" />
       <div className="mainContainer">
-      <Link href="/work/designflows">
-        <div className={` ${'row'} , ${styles.caseStudy}`}>
-          <div className="column">
-            <img src="/169competition.png" alt="Designflows 2021 - Coming Soon" className={styles.imgRound} />
+        <Link href="/work/designflows">
+          <div className={` ${'row'} , ${styles.caseStudy}`}>
+            <div className="column">
+              <img src="/169competition.png" alt="Designflows 2021 - Coming Soon" className={styles.imgRound} />
+            </div>
+            <div className="column">
+              <div className={styles.csTitle}>Designflows 2021</div>
+              <h3>Tool Hire App</h3>
+              <p>A competition project to create a unique UI within a short amount of time.</p>
+              <button className="secondary">View Case Study &#x2192;</button>
+            </div>
+
           </div>
-          <div className="column">
-            <div className={styles.csTitle}>Designflows 2021</div>
-            <h3>Tool Hire App</h3>
-            <p>A competition project to create a unique UI within a short amount of time.</p>
-            <button className="secondary">View Case Study &#x2192;</button>
-          </div>
-          
-        </div> 
         </Link>
         <Link href="/work/offer-management">
-        <div className={` ${'row'} , ${styles.caseStudy}`}>
-          <div className="column">
-            <img src="/169offers.png" alt="Offer management section of the new dashboard for Rakuten Advertising" className={styles.imgRound} />
+          <div className={` ${'row'} , ${styles.caseStudy}`}>
+            <div className="column">
+              <img src="/169offers.png" alt="Offer management section of the new dashboard for Rakuten Advertising" className={styles.imgRound} />
+            </div>
+            <div className="column">
+              <div className={styles.csTitle}>Rakuten Advertising</div>
+              <h3>Offer Management</h3>
+              <p>The updating of a legacy application to offer a modern experience for the user.</p>
+              <button className="secondary">View Case Study &#x2192;</button>
+            </div>
           </div>
-          <div className="column">
-            <div className={styles.csTitle}>Rakuten Advertising</div>
-            <h3>Offer Management</h3>
-            <p>The updating of a legacy application to offer a modern experience for the user.</p>
-            <button className="secondary">View Case Study &#x2192;</button>
-          </div>
-        </div>
         </Link>
         {/* <Link href="/work/brewtiful"> */}
         <div className={` ${'row'} , ${styles.caseStudy}`}>
