545256c16fcfb29afde59a3ff9d334d75c5e2715
tomatronic <tom.spencer@rakuten.com>
2022-01-19 20:40:55 +0000

    Adding footer back in

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,7 @@
 import styles from "../pages/css/footer.module.css"
-
+import Email from "/public/footer/email.svg"
+import Linkedin from "/public/footer/linkedin.svg"
+import Dribbble from "/public/footer/dribbble.svg"
 
 function Footer() {
     return <>
@@ -10,7 +12,9 @@ function Footer() {
                             <b>Let's connect</b>
                             </div>
                                 <div className={styles.socials}>
-                                    This is new  
+                                    <a href="mailto:tom.m.spencer@gmail.com?subject=Portfolio%20Enquiry"><Email className={styles.email} /></a>
+                                    <a href="https://dribbble.com/tomatronic" target="_blank" rel="noreferrer"><Dribbble className={styles.email} /></a>
+                                    <a href="https://www.linkedin.com/in/thomas-spencer/" target="_blank" rel="noreferrer"><Linkedin className={styles.email} /></a>                   
                                 </div>
                     Tom Spencer 2022                   
                 </div>
