0df4a5dffb66bccca2dfe4efc0355fa07b8d1b9c
tomatronic <tom.spencer@rakuten.com>
2022-01-20 18:45:18 +0000

    Adding brewtiful copy

--- pages/work/beer-blog.js
+++ pages/work/beer-blog.js
@@ -51,6 +51,25 @@ function beerBlog() {
                     </div>
                 </div>
             <div className="mainContainer">
+            <div className="row">
+                    <div className="column">
+                        <h1>A new brand is born</h1>
+                        <p>So now I had my idea in place I wanted to give it name. I wanted to be able to refer to this idea as a brand and not just a thing. I also new I needed to create my goal. So my next steps was to come up with a name and an end goal.</p>
+                        <p>I decided I wanted to highlight that the brand involved beer and design, I knew I wanted a name that was easy and played on words. After many hours of searching the internet, checking domain availabilities and social channels names I settled on the name 'Brewtiful'! More on this in a bit.</p>
+                        <br />
+                        <h1>Project Goal</h1>
+                        <p>Create a magazine/blog style website that focuses on craft beers and the packaging they are delivered in. Each beer will be reviewed in a way that entertains but also educates.</p>
+                    </div>
+                    <div className="column">Image</div>
+                </div>
+                <div className="row">
+                    <div className="column">
+                        <h1>Understanding the landscape</h1>
+                        <p>My initial task was to explore what options were already available. I explored lots of beer related websites to try and gain an understanding of what was available. I decided to place each site into a small matrix to highlight the content being offered. The matrix was created based on my initial idea, the first axis was to show how beer focused the site was, the second axis was to show how and if the design was highlighted.</p>
+                        <p>I quickly came to notice that nothing came close to the area I was envisioning this project. It seemed that most sites focused on one or the other. I would be aiming for something much more central.</p>
+                    </div>
+                    <div className="column">Image of matrix</div>
+                </div>
                 <div className="row">
                     <div className="column">
                         This page is currently being constructed, please check back soon...
