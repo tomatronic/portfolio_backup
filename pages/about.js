8a66ab8c76f4b14104b27c7a05ec85f95d7e493c
tomatronic <tom.spencer@rakuten.com>
2023-01-18 13:39:36 +0000

    Restyle about page test

--- pages/about.js
+++ pages/about.js
@@ -14,14 +14,35 @@ function About() {
             <NavBar />
             <div className="mainContainer">
                 <div className={styles.mainContainer}>
-                    <div className="reverse-row-sm-gap">
-                        <div className="column">
+                    <div className="row">
+                    <div className="column">
                             <img src="/bio.png" alt="You will often find me out hiking in the South Downs National Park" className={styles.bioImg} />
                         </div>
-                        <div className="nopad-double-column">                            
-                            <div className={styles.intro}>About</div>
+                    </div>
+                    <div className="row">
+                        <div className="column">
+                        <div className={styles.intro}>About</div>
+                        </div>
+                        <div className="column">
                             <p>Hello, I'm Tom and I have been working in design for a number of years now. I am now settled into the world of user experience but am still often found experimenting with UI components or even getting my hands dirty with some (limited) front end developement.</p>
                             <p>Being a UX designer allows me to solve complex problems and bring the end user a pleasurable experience. I make sure that products are enjoyable, accessible and beautiful to look at. I have a passion for getting fully immersed in a problem and experimenting with a range of possible solutions.</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="column">
+                        <div className={styles.intro}>Experience</div>
+                        </div>
+                        <div className="column">
+                            <p>UX Designer @ Rakuten Advertising</p>
+                            <p>Feb 2020 - Present</p>
+                            <p>Junior UX Designer @ Rakuten Advertising</p>
+                            <p>Jul 2018 - Feb 2020</p>
+                            <p>Senior Creative Developer @ Pure360</p>
+                            <p>Feb 2016 - Jul 2018</p>
+                            <p>Creative Developer @ Pure360</p>
+                            <p>Jun 2014 - Feb 2016</p>
+                            <p>Email Creative <span className="highlight">@ Friday Media Group</span></p>
+                            <p>Nov 2013 - Jun 2014</p>
                         </div>
                     </div>
                     <div className="notm-row">
