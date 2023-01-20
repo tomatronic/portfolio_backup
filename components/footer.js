47adcf619725466597f594a1f0d1036707bff770
tomatronic <tom.spencer@rakuten.com>
2023-01-20 16:28:29 +0000

    Footer reduction

--- components/footer.js
+++ components/footer.js
@@ -7,10 +7,7 @@ function Footer() {
     return <>
         <div className={styles.footer}>
             <div className="mainContainer">
-                <div className={styles.details}>
-                            <div>
-                            <b>Let's connect</b>
-                            </div>
+                <div className={styles.details}>                            
                                 <div className={styles.socials}>
                                     <a href="mailto:tom.m.spencer@gmail.com?subject=Portfolio%20Enquiry" aria-label="Email me"><Email className={styles.email} /></a>
                                     <a href="https://dribbble.com/tomatronic" target="_blank" rel="noreferrer" aria-label="Dribbble"><Dribbble className={styles.email} /></a>
