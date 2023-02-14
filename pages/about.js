bfff49c71ca96835c182dd712807f12ff4f60029
tomatronic <tom.spencer@rakuten.com>
2023-02-14 11:05:04 +0100

    Copy updates

--- pages/about.js
+++ pages/about.js
@@ -35,15 +35,15 @@ function About() {
                         <h1>Experience</h1>
                         </div>
                         <div className="double-column">
-                            <p>UX Designer <span className={styles.highlight}>&mdash; Feb 2020 - Present</span><br /><br />
+                            <p>UX Designer <span className={styles.highlight}>&mdash; Feb 2020 - Present</span><br />
                             <span className={styles.highlight}>Rakuten Advertising</span></p>
-                            <p>Junior UX Designer <span className={styles.highlight}>&mdash; Jul 2018 - Feb 2020</span><br /><br />
+                            <p>Junior UX Designer <span className={styles.highlight}>&mdash; Jul 2018 - Feb 2020</span><br />
                             <span className={styles.highlight}>Rakuten Advertising</span></p>
-                            <p>Senior Creative Developer <span className={styles.highlight}>&mdash; Feb 2016 - Jul 2018</span><br /><br />
+                            <p>Senior Creative Developer <span className={styles.highlight}>&mdash; Feb 2016 - Jul 2018</span><br />
                             <span className={styles.highlight}>Pure360</span></p>
-                            <p>Creative Developer <span className={styles.highlight}>&mdash; Jun 2014 - Feb 2016</span><br /><br />
+                            <p>Creative Developer <span className={styles.highlight}>&mdash; Jun 2014 - Feb 2016</span><br />
                             <span className={styles.highlight}>Pure360</span></p>
-                            <p>Email Creative <span className={styles.highlight}>&mdash; Nov 2013 - Jun 2014</span><br /><br />
+                            <p>Email Creative <span className={styles.highlight}>&mdash; Nov 2013 - Jun 2014</span><br />
                             <span className={styles.highlight}>Friday Media Group</span></p>
                         </div>
                     </div>
