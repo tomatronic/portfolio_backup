4a72577ab4f2ffda26854316140b7711aecec3ce
tomatronic <tom.spencer@rakuten.com>
2023-02-27 18:05:41 +0100

    zero opacity

--- pages/about.js
+++ pages/about.js
@@ -61,7 +61,7 @@ function About() {
             <motion.div
         initial={{ scaleX: 1 }}
         animate={{ scaleX: 0, transition: { duration: 0.5, ease: "circOut" } }}
-        exit={{ scaleX: 1, transition: { duration: 0.5, ease: "circIn" } }}
+        exit={{ scaleX: 1, transition: { duration: 0.5, ease: "circIn" }, opacity: 0 }}
         style={{ originX: isPresent ? 0 : 1 }}
         className="privacy-screen"
       />
