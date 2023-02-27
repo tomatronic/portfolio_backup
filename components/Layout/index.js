4aa69b1ae9a06139b7cba33c6b1bbc971c10e923
tomatronic <tom.spencer@rakuten.com>
2023-02-27 18:46:58 +0100

    Re-add animation

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 const Layout = ({ children }) => (
   <motion.div
-    initial={{ y: 100, opacity: 0 }}
+    initial={{ y: 70, opacity: 0 }}
     animate={{ y: 0, opacity: 1 }}
     // exit={{ y: 100, opacity: 0}}
     transition={{
