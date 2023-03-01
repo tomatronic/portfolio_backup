4ace332b067100558ed0e217feeeede3c7db44cf
tomatronic <tom.spencer@rakuten.com>
2023-03-01 15:17:09 +0100

    Updated footer to tailwind

--- pages/workWIP.js
+++ pages/workWIP.js
@@ -12,7 +12,7 @@ function WorkWIP() {
         <Head />
         <NavBar />
         <div className="container max-w-screen-lg mx-auto px-6 pt-10">
-            <p className="text-4xl">Work</p>       
+            <p className="text-4xl font-semibold">Work</p>       
             <p>The following is a small collection of projects I have contributed to and am particularly proud of.</p>
             {caseStudies.map((e)=>{
        return (
