9b11d0b170cec39a4c477317884e9d3f6c5540e3
tomatronic <tom.spencer@rakuten.com>
2023-03-01 14:43:30 +0100

    Updated nav component to tailwind

--- components/navbar.js
+++ components/navbar.js
@@ -28,31 +28,3 @@ function NavBar() {
   }
   
 export default NavBar
-
-  /* function NavBar() {
-    return <>
-    <div className={styles['site-header']}>
-      <div className={`${styles.wrapper} , ${styles['site-header__wrapper']}`}>
-            <Link href="/">
-              <a link="/" className={styles.logo}>TOM SPENCER</a>
-            </Link>
-            <nav className={styles.nav}>
-              <ul>
-                <li>     
-                  <ActiveLink activeClassName="active" href="/#work"><a href="/#work"> Work </a></ActiveLink> 
-                </li>
-                <li>          
-                  <ActiveLink activeClassName="active" href="/about"><a link="/about"> About </a></ActiveLink>  
-                </li>
-                <li>
-                  <ActiveLink activeClassName="active" href="/resume.pdf"><a link="/resume.pdf" target="_blank" rel="noopener noreferrer"> Resumé </a></ActiveLink>
-                </li>
-              </ul>                      
-            </nav>
-        </div>
-    </div>
-    </>
-    
-  }
-  
-  export default NavBar */
