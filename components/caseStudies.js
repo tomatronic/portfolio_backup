77043e901521c3c48b7e8f509673144bd92317f9
tomatronic <tom.spencer@rakuten.com>
2023-03-07 15:19:52 +0000

    Reorder case studies

--- components/caseStudies.js
+++ components/caseStudies.js
@@ -7,18 +7,18 @@ const caseStudies=[
       img: "/offersThumb.png"
     },
     {
-        tag: "Designflows 2021",
-        title:"Improving the offer management invite experience",
-        blurb:"Product design, User research, Brand creation",
-        link:"/work/designflows",
-        img: "/competitionThumb.png"
-    },
-    {
         tag: "Brewtiful",
         title:"Improving the offer management invite experience",
         blurb:"Product design, User research, Brand creation, Front-end development",
         link:"/work/beer-blog",
         img: "/brewtifulThumb.png"
     },
+    {
+      tag: "Designflows 2021",
+      title:"Improving the offer management invite experience",
+      blurb:"Product design, User research, Brand creation",
+      link:"/work/designflows",
+      img: "/competitionThumb.png"
+  },
   ]
   export default caseStudies;
