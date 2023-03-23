c7ab5aa33ee987654afd47ad75f214300eee02f6
tomatronic <tom.spencer@rakuten.com>
2023-03-23 16:49:43 +0000

    Adding alt tracking

--- pages/_app.js
+++ pages/_app.js
@@ -5,27 +5,11 @@ import 'react-medium-image-zoom/dist/styles.css'
 import Head from "next/head";
 import NavBar from "/components/navbar.js"
 import Footer from "/components/footer.js"
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import * as gtag from '/lib/gtag'
+import ReactGA from "react-ga4"
+
+ReactGA.initialize("G-CCDKVM70NV")
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on('routeChangeComplete', handleRouteChange)
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
 
   return (
     <>
