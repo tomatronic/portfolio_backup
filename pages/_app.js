fa90b3453ef15ff2914f83c622fc4a5147fefd5b
tomatronic <tom.spencer@rakuten.com>
2023-03-23 15:43:04 +0000

    New gtag method

--- pages/_app.js
+++ pages/_app.js
@@ -1,48 +1,34 @@
 import React from 'react';
-import { AppProps } from 'next/app';
 import "../styles.css";
 import 'react-medium-image-zoom/dist/styles.css'
-import Script from "next/script";
 import Head from "next/head";
 import NavBar from "/components/navbar.js";
 import Footer from "/components/footer.js";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import * as gtag from "../lib/gtag";
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
+import * as ga from '../lib/ga'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
+function MyApp({ Component, pageProps }) {
+  const router = useRouter()
 
   useEffect(() => {
     const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
 
-    router.events.on("routeChangeComplete", handleRouteChange);
-
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
     return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   return (
     <>
-      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-CCDKVM70NV"></Script>
-      <Script
-        id='google-analytics'
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-CCDKVM70NV', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
       <Head>
         <title>Tom Spencer - UX/UI Designer</title>
         <meta property="og:title" content="Tom Spencer - UX / UI Designer" />
