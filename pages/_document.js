f7d2ab6c8ec4bb8483e110e8305e2c380fa6db47
tomatronic <tom.spencer@rakuten.com>
2023-03-23 16:34:43 +0000

    Reduce image size

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
     render() {
       return (
         <Html>
@@ -31,3 +31,5 @@ export default class MyDocument extends Document {
       )
     }
   }
+
+  export default MyDocument;
