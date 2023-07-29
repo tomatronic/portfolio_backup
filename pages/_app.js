f5b08b9234ffc0ebdfa8591c4bc29c8a82bee627
tomatronic <tom.spencer@rakuten.com>
2023-07-29 16:41:33 +0100

    Adding additional meta data

--- pages/_app.js
+++ pages/_app.js
@@ -19,8 +19,19 @@ function MyApp({ Component, pageProps, router }) {
         <meta charSet="UTF-8" />
         <meta name="keywords" content="UX, User Experience, UI, User Interface, Tom Spencer, UK, Brighton" />
         <meta name="author" content="Tom Spencer" />
-        <meta name="description" content="Hi, I'm Tom Spencer. UX/UI designer focused on delivering effective user centric designs that drive engagement. Currently UX Designer @ Rakuten Advertising." />
+        <meta name="description" content="UX/UI designer focused on delivering effective user centric designs that drive engagement. Currently UX Designer @ Rakuten Advertising." />
         <link rel="shortcut icon" href="/favicon.ico" />
+        <meta property="og:url" content="https://www.tomspencer.design" />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="UX/UI designer focused on delivering effective user centric designs that drive engagement. Currently UX Designer @ Rakuten Advertising." />
+        <meta property="og:image" content="https://www.tomspencer.design/competitionThumb.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="tomspencer.design" />
+        <meta property="twitter:url" content="https://www.tomspencer.design" />
+        <meta name="twitter:title" content="Tom Spencer - UX / UI Designer" />
+        <meta name="twitter:description" content="UX/UI designer focused on delivering effective user centric designs that drive engagement. Currently UX Designer @ Rakuten Advertising." />
+        <meta name="twitter:image" content="https://www.tomspencer.design/competitionThumb.png" />
+        
       </Head>
       <NavBar />
       <Component {...pageProps} key={router.asPath} />
