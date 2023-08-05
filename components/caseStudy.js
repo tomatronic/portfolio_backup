de9245fee01e1c83d860e2bccd4893eae1693a68
tomatronic <tom.spencer@rakuten.com>
2023-08-05 15:40:50 +0100

    New favicon

--- components/caseStudy.js
+++ components/caseStudy.js
@@ -7,8 +7,8 @@ import InView from './Layout/InView';
 function Presentation(props) {
   return (
 
-      <div className="flex-1 flex flex-col justify-between overflow-hidden rounded-2xl bg-white border shadow-[0px_1px_8px_rgba(13,34,71,0.12),_0px_28px_96px_rgba(13,34,71,0.1)] hover:shadow-[0px_28px_96px_rgba(99,102,241,0.2)]">
-
+      <div className="flex-1 flex flex-col justify-between overflow-hidden rounded-2xl bg-white border shadow-[0px_1px_8px_rgba(13,34,71,0.12),_0px_28px_96px_rgba(13,34,71,0.1)]">
+        <Link href={props.link}>
         <div className="">
           <InView>
             <Link href={props.link}><img src={props.img} alt={props.title} className="object-cover ease-in-out duration-500 hover:scale-110 cursor-pointer" /></Link>
@@ -19,9 +19,9 @@ function Presentation(props) {
             <div className="text-gray-500 uppercase font-semibold text-xs">{props.tag}</div>
             <div className='text-lg font-medium tracking-tight'>{props.title}</div>
             <p className='text-sm'>{props.blurb}</p>
-            <Link href={props.link}><span className="text-indigo-500">Read case study</span></Link>
+            <span className="text-indigo-500">Read case study</span>
           </div>
-        
+        </Link>
 
       </div>
   
