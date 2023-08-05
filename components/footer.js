612a38ad3ee16afd28159fc23119f4b147c8139a
tomatronic <tom.spencer@rakuten.com>
2023-08-05 17:13:04 +0100

    Design Flows page better

--- components/footer.js
+++ components/footer.js
@@ -8,9 +8,9 @@ function Footer() {
          <div className="container max-w-screen-lg mx-auto border-t border-gray-200 text-gray-700 py-6">
                 <div className="flex flex-col text-center">                            
                                 <div className="flex gap-6 max-w-[120px] mx-auto">
-                                    <a href="mailto:tom.m.spencer@gmail.com?subject=Portfolio%20Enquiry" aria-label="Email me"><Email className="w-6 h-6 fill-gray-700 hover:fill-purple-500" /></a>
-                                    <a href="https://dribbble.com/tomatronic" target="_blank" rel="noreferrer" aria-label="Dribbble"><Dribbble className="w-6 h-6 fill-gray-700 hover:fill-purple-500" /></a>
-                                    <a href="https://www.linkedin.com/in/thomas-spencer/" target="_blank" aria-label="LinkedIn" rel="noreferrer"><Linkedin className="w-6 h-6 fill-gray-700 hover:fill-purple-500" /></a>                   
+                                    <a href="mailto:tom.m.spencer@gmail.com?subject=Portfolio%20Enquiry" aria-label="Email me"><Email className="w-6 h-6 fill-gray-700 hover:fill-indigo-500" /></a>
+                                    <a href="https://dribbble.com/tomatronic" target="_blank" rel="noreferrer" aria-label="Dribbble"><Dribbble className="w-6 h-6 fill-gray-700 hover:fill-indigo-500" /></a>
+                                    <a href="https://www.linkedin.com/in/thomas-spencer/" target="_blank" aria-label="LinkedIn" rel="noreferrer"><Linkedin className="w-6 h-6 fill-gray-700 hover:fill-indigo-500" /></a>                   
                                 </div>                                     
                 </div>
         </div>
