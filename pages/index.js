b485b3db1d8c542591c2623c0559c2509bb0f70b
tomatronic <tom.spencer@rakuten.com>
2023-08-05 15:54:42 +0100

    New favicon

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ function HomePage() {
       <Layout>
       <div className="container max-width-screen-xl mx-auto px-6 flex flex-col justify-left">
         <div className="h-full flex">
-          <div className="flex flex-row flex-wrap max-w-3xl mb-24 md:mb-36 md:mt-40">
+          <div className="flex flex-row flex-wrap max-w-3xl my-24 md:mb-36 md:mt-40">
             <div className="flex flex-col">                        
               <div className="text-2xl md:text-5xl leading-8">
                 <b className="text-transparent bg-clip-text bg-gradient-to-tr from-rose-400 via-fuchsia-500 to-indigo-500">UX Designer</b>
