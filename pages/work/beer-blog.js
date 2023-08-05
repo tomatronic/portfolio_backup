5d34e8c8342a5cfc4d2f8244d386686c7ed35591
tomatronic <tom.spencer@rakuten.com>
2023-08-05 17:06:40 +0100

    Design Flows page better

--- pages/work/beer-blog.js
+++ pages/work/beer-blog.js
@@ -29,31 +29,32 @@ function BeerBlog() {
                             <p className="pb-6 border-b border-gray-200">Planning, Competitor research, Brand creation, Information architecture, Prototyping, User testing, Hi-fi mockups, Logo design</p>
                         </div>
                         <div className="col-span-3 pb-10">
+                        <p><b>Note</b>: Project under review - New iteration soon.</p>
                             <h2>About the project</h2>
                             <p>I decided I wanted to find a new project for myself. I wanted to create something that would both challenge me as a designer but also still provide a solution to a problem.</p>
-                            <p>So what did I decide? After spending a long time thinking about what I enjoyed and how I may be able to transform this into a project I came up with the concept of 'Brewtiful'. The basis to the idea is that craft beers often feature great designs on the cans and bottles they are distrubuted in. I noticed that although there are many websites and apps out there that focus on the beer itself very few mentioned the design effort.</p>
+                            <p className="mb-16">So what did I decide? After spending a long time thinking about what I enjoyed and how I may be able to transform this into a project I came up with the concept of 'Brewtiful'. The basis to the idea is that craft beers often feature great designs on the cans and bottles they are distrubuted in. I noticed that although there are many websites and apps out there that focus on the beer itself very few mentioned the design effort.</p>
                             <h2>Project Goal</h2>
-                            <p>Create a magazine/blog style website that focuses on craft beers and the packaging they are delivered in. Each beer will be reviewed in a way that entertains but also educates.</p>
+                            <p className="mb-16">Create a magazine/blog style website that focuses on craft beers and the packaging they are delivered in. Each beer will be reviewed in a way that entertains but also educates.</p>
                             <Zoom>
                                 <Image src="/sampleBeer.png" layout="intrinsic" width="500" height="542" alt="I had previously begun exploring ways of documenting beers I had sampled" />
                             </Zoom>
-                            <h2 className="pt-5">A new brand is born</h2>
+                            <h2 className="pt-16">A new brand is born</h2>
                             <p>So now I had my idea in place I wanted to give it name. I wanted to be able to refer to this idea as a brand and not just a thing. I also new I needed to create my goal. So my next steps was to come up with a name and an end goal.</p>
-                            <p>I decided I wanted to highlight that the brand involved beer and design, I knew I wanted a name that was easy and played on words. After many hours of searching the internet, checking domain availabilities and social channels names I settled on the name 'Brewtiful'! More on this in a bit.</p>
+                            <p className="mb-16">I decided I wanted to highlight that the brand involved beer and design, I knew I wanted a name that was easy and played on words. After many hours of searching the internet, checking domain availabilities and social channels names I settled on the name 'Brewtiful'! More on this in a bit.</p>
                             <Zoom>
                                 <Image src="/initialDoodle.png" layout="intrinsic" width="1000" height="774" alt="Some very messy initial thoughts on what I wanted to include and the direction I could take a name" />
                             </Zoom>
-                            <h2 className="pt-5">Understanding the landscape</h2>
+                            <h2 className="pt-16">Understanding the landscape</h2>
                             <p>My initial task was to explore what options were already available to try and gain an understanding of where my idea may sit.</p>
-                            <p>I collated a few of the sites into a list and identifed where they were similar and if they offered any unique selling points. I then plotted my proposal against these findings.</p>
-                            <div className="pb-5">
+                            <p className="mb-16">I collated a few of the sites into a list and identifed where they were similar and if they offered any unique selling points. I then plotted my proposal against these findings.</p>
+                            <div className="pb-16">
                                 <Zoom>
                                     <Image src="/brewComp.png" layout="intrinsic" width="2390" height="1206" alt="Competitor research - Investigating the craft beer and design review landscape" />
                                 </Zoom>
                             </div>
                             <div className="max-w-full bg-amber-300 rounded-lg p-10 py-5">
                                 <h2>Who is this for?</h2>
-                                <p>My next task was to work out who my audience was and what they might want to get from this new site. As this is a personal project my access to research is a little more limited, but I went ahead and created a couple of personas based on data I could find. I focused on the two sides of this project, the beer lover and the design lover. The end goal is to create a project that an be enjoyed by both parties. Creating these personas will help ensure I stay as user centric as possible.</p>
+                                <p className="mb-16">My next task was to work out who my audience was and what they might want to get from this new site. As this is a personal project my access to research is a little more limited, but I went ahead and created a couple of personas based on data I could find. I focused on the two sides of this project, the beer lover and the design lover. The end goal is to create a project that an be enjoyed by both parties. Creating these personas will help ensure I stay as user centric as possible.</p>
                                 <Zoom>
                                     <Image src="/beerLover.png" layout="intrinsic" width="1684" height="1190" alt="User Persona A - The user persona of a beer lover" />
                                 </Zoom>
@@ -61,7 +62,7 @@ function BeerBlog() {
                                     <Image src="/designLover.png" layout="intrinsic" width="1684" height="1190" alt="User Persona B - The user persona of a design lover" />
                                 </Zoom>
                             </div>
-                            <h2 className="pt-5">Conidering content</h2>
+                            <h2 className="pt-16">Conidering content</h2>
                             <p>Now was time to start thinking about what content would make up this site.</p>
                             <p>In order to start sketching some ideas I decided to plan out a rough IA (Information Architecture) in order to identify the pages that would need designing.</p>
                             <p>The site would likely be fairly simple and built in a way that I could reuse a template for multiple pages. I wanted to ensure that adding new content was not a labourious task. With this in mind I identified 5-6 main pages/templates I would require:</p>
@@ -73,49 +74,48 @@ function BeerBlog() {
                                 &bull; <b>Blog</b> - This can be kept fairly simple. The main focus is the beer!<br />
                                 &bull;  <b>Resource pages</b> - This includes 'about', 'contact' etc.
                             </p>
-                            <p>At this point I have a good idea about the structure of the site. It's time to start thinking about design.</p>
+                            <p className="mb-16">At this point I have a good idea about the structure of the site. It's time to start thinking about design.</p>
                             <Zoom>
                                 <Image src="/brewtifulIA.png" layout="intrinsic" width="1392" height="622" alt="My inital decision for the IA of the new site. Kept a fairly flat IA." />
                             </Zoom>
-                            <h2 className="pt-5">Initial sketches</h2>
+                            <h2 className="pt-16">Initial sketches</h2>
                             <p>I decided to take pen to paper again at this point and begin sketching some ideas. I wanted to get a rough idea of what may work and aimed to identify information that may or may not be needed on the pages.</p>
-                            <p>The idea of quick scribbles like this allows me to perform multiple iterations in minimal time. It also helps begin the process of creating more of a brand identity.</p>
+                            <p className="mb-16">The idea of quick scribbles like this allows me to perform multiple iterations in minimal time. It also helps begin the process of creating more of a brand identity.</p>
                             <Zoom>
                                 <Image src="/initialSketchBrewtiful.png" layout="intrinsic" width="1000" height="1000" alt="I doodled quite a bit for this project, here is a sample of some of my ideas." />
                             </Zoom>
-                            <h2 className="pt-5">Brand identity</h2>
+                            <h2 className="pt-16">Brand identity</h2>
                             <p>I decided to take pen to paper again at this point and begin sketching some ideas. I wanted to get a rough idea of what may work and aimed to identify information that may or may not be needed on the pages.</p>
-                            <p>The idea of quick scribbles like this allows me to perform multiple iterations in minimal time. It also helps begin the process of creating more of a brand identity.</p>
+                            <p className="mb-16">The idea of quick scribbles like this allows me to perform multiple iterations in minimal time. It also helps begin the process of creating more of a brand identity.</p>
                             <Zoom>
                                 <Image src="/beerID.png" layout="intrinsic" width="1545" height="1152" alt="I created a simple colour palette and chose two fonts to identify my brand." />
                             </Zoom>
-                            <h2 className="pt-5">Low fidelity mocks</h2>
+                            <h2 className="pt-16">Low fidelity mocks</h2>
                             <p>I felt at this point that the project was at a good point. And although not a huge step I decided to grab my favourite of the previous sketches and reproduce them in an ever so slightly better version.</p>
-                            <p>This new version of the mockups helped me further understad how the final page may look. A few tweaks added here and I am strting to be very happy with the design direction.</p>
+                            <p className="mb-16">This new version of the mockups helped me further understad how the final page may look. A few tweaks added here and I am strting to be very happy with the design direction.</p>
                             <Zoom>
                                 <Image src="/lofiBeer.png" layout="intrinsic" width="2000" height="1500" alt="Developing sketches into some very simple digital assets" />
                             </Zoom>
-                            <h2 className="pt-5">Prototyping</h2>
+                            <h2 className="pt-16">Prototyping</h2>
                             <p>I followed a slightly alternate route to what I would usually. I decided to go ahead and create a few prototypes that I could then try and gather some feedback on.</p>
-                            <p>At this point I wasnt sure how I would go about getting some testing completed, or gather feedback. I didnt want to rely on friends and collegaues due to the risk of bias opinion. I opted to try reddit... Not a conventional approach and arguably not much better than asking friends and family as I dont know the background of those that respond.</p>
+                            <p className="mb-16">At this point I wasnt sure how I would go about getting some testing completed, or gather feedback. I didnt want to rely on friends and collegaues due to the risk of bias opinion. I opted to try reddit... Not a conventional approach and arguably not much better than asking friends and family as I dont know the background of those that respond.</p>
                             <Zoom>
                                 <Image src="/review.png" layout="intrinsic" width="1212" height="784" alt="Beer review - An early example of how a review may be displayed" />
                             </Zoom>
-                            <h2 className="pt-5">Feedback</h2>
+                            <h2 className="pt-16">Feedback</h2>
                             <p>My approach of using reddit was not something I would usually do. It is not focused enough to gain true user insights and instead relies on the people commenting understanding the use case and having some design knowledge.</p>
-                            <p>However, ignoring the above, the response from the unknown people reviewing my UI was positive. This was reassuring and also gave me some ideas for iterations.</p>
+                            <p className="mb-16">However, ignoring the above, the response from the unknown people reviewing my UI was positive. This was reassuring and also gave me some ideas for iterations.</p>
                             <Zoom>
                                 <Image src="/crit.png" layout="intrinsic" width="1212" height="784" alt="Examples of replies and feedback from reddit" />
                             </Zoom>
-                            <h2 className="pt-5">Final mocks</h2>
-                            <p>Armed with some feedback from the reddit community I decided to go ahead and make a few tweaks before settling on a design.</p>
+                            <h2 className="pt-16">Final mocks</h2>
+                            <p className="mb-16">Armed with some feedback from the reddit community I decided to go ahead and make a few tweaks before settling on a design.</p>
                             <Zoom>
                                 <Image src="/brewtiful.png" layout="intrinsic" width="1212" height="784" alt="Brewtiful - The current hi-fidelity mocks" />
                             </Zoom>
-                            <h2 className="pt-5">Project reflection</h2>
+                            <h2 className="pt-16">Project reflection</h2>
                             <p>Being a personal project I am happy with where I have got to. I hope in the future to iterate on this further and maybe even take a dive into building this into a real life website.</p>
                             <p>The design process required a bit more discipline than other projects I have worked on as I had to find time around my work and social life. I think if I embark on a similar project in the future I will make sure to plan time each week to focus solely on the idea.</p>
-
                         </div>
                     </div>
                 </div>
@@ -123,7 +123,7 @@ function BeerBlog() {
                     <div className="flex flex-row flex-wrap">
                         <div className="flex flex-col flex-1 group cursor-pointer text-right justify-items-end content-end">
                             <Link href="/work/offer-management">
-                                <div className="group-hover:bg-purple-500 p-10 ease-in-out duration-300">
+                                <div className="group-hover:bg-indigo-500 p-10 ease-in-out duration-300">
                                     <div className="group-hover:text-white ease-in-out duration-300">
                                         <div className="text-gray-500 mb-1 uppercase font-semibold text-sm group-hover:text-white ease-in-out duration-300">Previous Project</div>
                                         <h2>Offer management</h2>
@@ -133,7 +133,7 @@ function BeerBlog() {
                         </div>
                         <div className="flex flex-col flex-1 group cursor-pointer">
                             <Link href="/work/designflows">
-                                <div className=" group-hover:bg-purple-500 p-10 ease-in-out duration-300">
+                                <div className=" group-hover:bg-indigo-500 p-10 ease-in-out duration-300">
                                     <div className="group-hover:text-white ease-in-out duration-300">
                                         <div className="text-gray-500 mb-1 uppercase font-semibold text-sm group-hover:text-white ease-in-out duration-300">Next Project</div>
                                         <h2>Designflows</h2>
