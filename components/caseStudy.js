c2112c4dad57be1d45dde9d2acc1681b9390c071
tomatronic <tom.spencer@rakuten.com>
2023-08-05 11:16:27 +0100

    Shrinking home page

--- components/caseStudy.js
+++ components/caseStudy.js
@@ -9,17 +9,17 @@ function Presentation(props) {
 <InView>
       <div className="flex-1 flex flex-col justify-between overflow-hidden rounded-2xl bg-white gap-10 pb-20">
 
-        <div className="basis-2/3 overflow-hidden rounded-2xl">
+        <div className="">
           <Link href={props.link}><img src={props.img} alt={props.title} className="object-cover ease-in-out duration-500 hover:scale-110 cursor-pointer" /></Link>
         </div>
-        <div className="basis-1/3">
+        
           <div>
             <div className="text-gray-500 mb-1 uppercase font-semibold text-sm">{props.tag}</div>
             <h2>{props.title}</h2>
             <p className='text-base'>{props.blurb}</p>
             <Link href={props.link}><span className="text-purple-600">Read case study</span></Link>
           </div>
-        </div>
+        
 
       </div>
   </InView>
