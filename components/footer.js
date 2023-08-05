5b22e1f140cbe006c413979ff4dbcdbd398b1634
tomatronic <tom.spencer@rakuten.com>
2023-08-05 19:48:54 +0100

    Simple divider

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,8 @@ import Dribbble from "/public/footer/dribbble.svg"
 function Footer() {
     return <>
      <div className="bg-slate-100">
-         <div className="container max-w-screen-lg mx-auto border-t border-gray-200 text-gray-700 py-6">
+         <div className="container max-w-screen-lg mx-auto text-gray-700 py-6">
+         <hr class="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-gray-200 to-transparent opacity-25 dark:opacity-100" />
                 <div className="flex flex-col text-center">                            
                                 <div className="flex gap-6 max-w-[120px] mx-auto">
                                     <a href="mailto:tom.m.spencer@gmail.com?subject=Portfolio%20Enquiry" aria-label="Email me"><Email className="w-6 h-6 fill-gray-700 hover:fill-indigo-500" /></a>
