10c16b343766b82d194fa8fbb367de1e7e5144a1
tomatronic <tom.spencer@rakuten.com>
2023-08-05 11:15:23 +0100

    Shrinking home page

--- components/caseStudy.js
+++ components/caseStudy.js
@@ -7,7 +7,7 @@ import InView from './Layout/InView';
 function Presentation(props) {
   return (
 <InView>
-      <div className="flex flex-col md:flex-row gap-10 pb-20">
+      <div className="flex-1 flex flex-col justify-between overflow-hidden rounded-2xl bg-white gap-10 pb-20">
 
         <div className="basis-2/3 overflow-hidden rounded-2xl">
           <Link href={props.link}><img src={props.img} alt={props.title} className="object-cover ease-in-out duration-500 hover:scale-110 cursor-pointer" /></Link>
