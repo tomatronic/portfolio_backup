9ef86e767d3201470fc7bccc6cb8f06defeca528
tomatronic <tom.spencer@rakuten.com>
2023-08-05 11:52:48 +0100

    Shrinking home page

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,8 @@ import Dribbble from "/public/footer/dribbble.svg"
 
 function Footer() {
     return <>
-         <div className="container max-w-screen-lg mx-auto border-t border-gray-200 text-gray-700 py-6 bg-slate-100">
+     <div className="bg-slate-100">
+         <div className="container max-w-screen-lg mx-auto border-t border-gray-200 text-gray-700 py-6">
                 <div className="flex flex-col text-center">                            
                                 <div className="flex gap-6 max-w-[120px] mx-auto">
                                     <a href="mailto:tom.m.spencer@gmail.com?subject=Portfolio%20Enquiry" aria-label="Email me"><Email className="w-6 h-6 fill-gray-700 hover:fill-purple-500" /></a>
@@ -12,6 +13,7 @@ function Footer() {
                                     <a href="https://www.linkedin.com/in/thomas-spencer/" target="_blank" aria-label="LinkedIn" rel="noreferrer"><Linkedin className="w-6 h-6 fill-gray-700 hover:fill-purple-500" /></a>                   
                                 </div>                                     
                 </div>
+        </div>
         </div>
     </>
   }
