89b2c67837c4c4e24a464b0543667c33cbee68fa
tomatronic <tom.spencer@rakuten.com>
2023-08-10 10:29:11 +0100

    Update all missing bits

--- pages/index.js
+++ pages/index.js
@@ -22,40 +22,40 @@ function HomePage() {
 
   return (
     <>
-    <div className="mb-10">
-      
-      <div>
-      <Layout>
-      <div className="container mx-auto px-6 flex flex-col justify-center md:justify-left">
-        <div className="h-full flex">
-          <div className="text-center md:text-left flex flex-row flex-wrap max-w-s md:max-w-3xl my-16 md:mb-36 md:mt-40">
-            <div className="flex flex-col">                        
-              <div className="text-2xl md:text-5xl leading-7 tracking-tight">
-                <b className="text-transparent bg-clip-text bg-gradient-to-tr from-rose-400 via-fuchsia-500 to-indigo-500">UX Designer</b>
-                &nbsp;focused on delivering simple and effective user centric designs.<br /><br />
+      <div className="mb-10">
+
+        <div>
+          <Layout>
+            <div className="container mx-auto px-6 flex flex-col justify-center md:justify-left">
+              <div className="h-full flex">
+                <div className="text-center md:text-left flex flex-row flex-wrap max-w-s md:max-w-3xl my-16 md:mb-36 md:mt-40">
+                  <div className="flex flex-col">
+                    <div className="text-2xl md:text-5xl leading-7 tracking-tight">
+                      <b className="text-transparent bg-clip-text bg-gradient-to-tr from-rose-400 via-fuchsia-500 to-indigo-500">UX Designer</b>
+                      &nbsp;focused on delivering simple and effective user centric designs.<br /><br />
+                    </div>
+                    <div className="text-base">Currently UX Designer @ <b><a href="https://www.rakutenadvertising.com" target="_blank" rel="noreferrer">Rakuten Advertising</a></b>
+                    </div>
+                  </div>
                 </div>
-                <div className="text-base">Currently UX Designer @ <b><a href="https://www.rakutenadvertising.com" target="_blank" rel="noreferrer">Rakuten Advertising</a></b>
-              </div>          
+              </div>
+            </div>
+          </Layout>
+          <a name="work" id="work" />
+          <div className="container mx-auto px-6">
+            <div className="flex flex-col gap-8 md:flex-row md:flex-wrap">
+              {caseStudies.map((e) => {
+                return (
+                  <Presentation key={e.link} tag={e.tag} title={e.title} blurb={e.blurb} img={e.img} link={e.link} />
+                );
+              })}
             </div>
           </div>
         </div>
       </div>
-      </Layout>  
-      <a name="work" id="work" />    
-      <div className="container mx-auto px-6">
-      <div className="flex flex-col gap-8 md:flex-row md:flex-wrap">
-      {caseStudies.map((e)=>{
-       return (
-        <Presentation key={e.link} tag={e.tag} title={e.title} blurb={e.blurb} img={e.img} link={e.link} />
-                );
-       })}
-       </div>
-      </div>
-    </div>
-    </div>
     </>
   )
 }
 
-export default HomePage   
-      
+export default HomePage
+
