de90a87260e28c1e0bcdc7dd33754f1393b56ebe
tomatronic <tom.spencer@rakuten.com>
2023-08-18 08:47:03 +0100

    Remove footer

--- components/footer.js
+++ components/footer.js
@@ -9,9 +9,7 @@ function Footer() {
          <hr className="mb-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-50 dark:opacity-100" />
                 <div className="flex flex-col text-center">                            
                                 <div className="flex gap-6 max-w-[120px] mx-auto">
-                                    <a href="mailto:tom.m.spencer@gmail.com?subject=Portfolio%20Enquiry" aria-label="Email me"><Email className="w-6 h-6 fill-gray-700 hover:fill-indigo-500" /></a>
-                                    <a href="https://dribbble.com/tomatronic" target="_blank" rel="noreferrer" aria-label="Dribbble"><Dribbble className="w-6 h-6 fill-gray-700 hover:fill-indigo-500" /></a>
-                                    <a href="https://www.linkedin.com/in/thomas-spencer/" target="_blank" aria-label="LinkedIn" rel="noreferrer"><Linkedin className="w-6 h-6 fill-gray-700 hover:fill-indigo-500" /></a>                   
+                                    <p>Tom Spencer - 2023</p>              
                                 </div>                                     
                 </div>
         </div>
