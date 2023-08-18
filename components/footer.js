9635710aeaabe6ab6c8cf9f51c46886673e2d8ce
tomatronic <tom.spencer@rakuten.com>
2023-08-18 10:46:30 +0100

    Edit image types

--- components/footer.js
+++ components/footer.js
@@ -4,8 +4,8 @@ function Footer() {
          <div className="container mx-auto text-gray-700 pb-6">
          <hr className="mb-6 h-px border-t-0 bg-slate-300" />
                 <div className="flex flex-col">                            
-                                <div className="flex gap-6 max-w-[180px] mx-auto text-sm">
-                                    Tom Spencer - 2023            
+                                <div className="flex gap-6 max-w-[180px] text-sm">
+                                   Designed and built by Tom Spencer - 2023            
                                 </div>                                     
                 </div>
         </div>
