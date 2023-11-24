498b89d69a89c73c4762e72dda5519214f480dd8
tomatronic <tom.spencer@rakuten.com>
2023-11-24 10:55:44 +0000

    New case study layout

--- components/footer.js
+++ components/footer.js
@@ -1,16 +1,16 @@
 function Footer() {
     return <>
-     <div className="bg-slate-100">
-         <div className="container mx-auto text-gray-700 pb-6">
-         <hr className="mb-6 h-px border-t-0 bg-slate-300" />
-                <div className="flex flex-col">                            
-                                <div className="flex gap-6 text-sm">
-                                   Designed and built by Tom Spencer - 2023            
-                                </div>                                     
+        <div className="bg-slate-100">
+            <div className="container mx-auto text-gray-700 pb-6">
+                <hr className="mb-6 h-px border-t-0 bg-slate-300" />
+                <div className="flex flex-col">
+                    <div className="text-sm text-center">
+                        Designed and built by Tom Spencer - 2023
+                    </div>
                 </div>
-        </div>
+            </div>
         </div>
     </>
-  }
-  
-  export default Footer
+}
+
+export default Footer
