9129135b7dda041eefcba9c9b55b4f48dc9bb9c8
tomatronic <tom.spencer@rakuten.com>
2023-11-24 11:24:36 +0000

    New case study layout

--- components/caseStudy.js
+++ components/caseStudy.js
@@ -6,19 +6,19 @@ import Image from 'next/image'
 function Presentation(props) {
   return (
 
-      <div className="flex-1 flex flex-col justify-between overflow-hidden rounded-2xl bg-white border shadow-[0px_1px_8px_rgba(13,34,71,0.12),_0px_28px_96px_rgba(13,34,71,0.1)]">
-
-        <div className="">
+      <div className="flex-1 flex flex-row justify-between overflow-hidden rounded-2xl bg-white border shadow-[0px_1px_8px_rgba(13,34,71,0.12),_0px_28px_96px_rgba(13,34,71,0.1)]">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-5 md:gap-0 auto-rows-auto">
+        <div className='flex col-span-2'>
             <Link href={props.link}><Image src={props.img} width={900} height={700} alt={props.title} priority={true} className="object-cover cursor-pointer" /></Link>
         </div>
         
-          <div className='mx-6 my-6 flex flex-col gap-2'>
+          <div className='mx-6 my-6 flex flex-col gap-2 col-span-2'>
             <div className="text-gray-500 uppercase font-semibold text-xs">{props.tag}</div>
             <div className='text-lg font-medium tracking-tight'>{props.title}</div>
             <p className='text-sm'>{props.blurb}</p>
             <Link href={props.link}><span className="text-indigo-600 hover:text-indigo-500 cursor-pointer tracking-tight">Read case study &#x2192;</span></Link>
           </div>
-        
+        </div>
 
       </div>
   
--- pages/index.js
+++ pages/index.js
@@ -27,15 +27,15 @@ function HomePage() {
 
         <div>
           <Layout>
-            <div className="container mx-auto px-6 flex flex-col justify-center md:justify-left">
+            <div className="container max-w-screen-md mx-auto px-6 flex flex-col justify-center">
               <div className="h-full flex">
                 <div className="text-center md:text-left flex flex-row flex-wrap max-w-s md:max-w-3xl my-16 md:mb-36 md:mt-40">
-                  <div className="flex flex-col">
-                    <div className="text-2xl md:text-5xl tracking-tight">
+                  <div className="flex flex-col text-center">
+                    <div className="text-2xl md:text-5xl md:leading-snug tracking-tight">
                       <b className="text-transparent bg-clip-text bg-gradient-to-tr from-rose-400 via-fuchsia-500 to-indigo-500">UX Designer</b>
-                      &nbsp;focused on delivering simple and effective user centric designs.<br /><br />
+                      &nbsp;focused on delivering simple, yet effective, user centric designs.<br />
                     </div>
-                    <div className="text-base">Currently UX Designer @ <b><a href="https://www.rakutenadvertising.com" target="_blank" rel="noreferrer">Rakuten Advertising</a></b>
+                    <div className="text-lg pt-10">Currently UX Designer @ <b><a href="https://www.rakutenadvertising.com" target="_blank" rel="noreferrer">Rakuten Advertising</a></b>
                     </div>
                   </div>
                 </div>
@@ -43,8 +43,8 @@ function HomePage() {
             </div>
           </Layout>
           <a name="work" id="work" />
-          <div className="container mx-auto px-6">
-            <div className="flex flex-col gap-8 md:flex-row md:flex-wrap">
+          <div className="container max-w-screen-md mx-auto px-6">
+            <div className="flex flex-col gap-8 md:flex-wrap">
               {caseStudies.map((e) => {
                 return (
                   <Presentation key={e.link} tag={e.tag} title={e.title} blurb={e.blurb} img={e.img} link={e.link} />
