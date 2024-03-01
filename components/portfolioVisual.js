33317a7b55a2b5059d75d840bc9dc304c723b4b0
Tom Spencer <tom.m.spencer@gmail.com>
2024-03-01 15:50:15 +0000

    Just trying to get to a solid point now

--- components/portfolioVisual.js
+++ components/portfolioVisual.js
@@ -50,11 +50,17 @@ const DotMatrix = ({ rows, columns, dotSize, gapSize, handleMouseMove }) => {
     return newDots;
   };
 
-  return (
+return (
+  <div
+    id="dot-container"
+    style={{ position: 'absolute', width: '100%' }}
+    onMouseMove={handleMouseMove}
+  >
     <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" style={{ pointerEvents: 'none' }}>
       {createDots()}
     </svg>
-  );
+  </div>
+);
 };
 
 export default DotMatrix;
