58a16d6e5aced0ee0c5d7ca9b0eb298bda82bb62
Tom Spencer <tom.m.spencer@gmail.com>
2024-03-01 17:34:30 +0000

    Add a null check

--- components/portfolioVisual.js
+++ components/portfolioVisual.js
@@ -38,7 +38,7 @@ const DotMatrix = ({ rows, columns, dotSize, gapSize }) => {
   for (let row = 0; row < rows; row++) {
     for (let col = 0; col < columns; col++) {
       const dotX = col * (dotSize * 2 + gapSize) + dotSize + gapSize;
-      const dotY = row * (dotSize * 2 + gapSize) + dotSize + gapSize - 50;
+      const dotY = row * (dotSize * 2 + gapSize) + dotSize + gapSize;
       const containerX = containerRect.left;
       const containerY = containerRect.top;
 
