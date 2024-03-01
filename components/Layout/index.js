e0dd7bccaa87486982a9962d305b1b08614df626
Tom Spencer <tom.m.spencer@gmail.com>
2024-03-01 17:46:03 +0000

    Remove animation

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 
-const Layout = ({ children }) => (
+/* const Layout = ({ children }) => (
   <motion.div
     initial={{ y: 80, opacity: 0 }}
     animate={{ y: 0, opacity: 1 }}
@@ -14,5 +14,5 @@ const Layout = ({ children }) => (
   >
     {children}
   </motion.div>
-);
+); */
 export default Layout;
