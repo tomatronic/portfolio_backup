7dcf5b39e001a26f3c9fb90fce3396b219f10dc3
Tom Spencer <tom.m.spencer@gmail.com>
2024-03-01 12:53:53 +0000

    Testing dots

--- pages/index.js
+++ pages/index.js
@@ -32,8 +32,7 @@ function HomePage() {
               <DotMatrix rows={200} columns={100} dotSize={2} gapSize={12} style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0, zIndex: 1 }} />
 
               
-              <div className="container max-w-screen-md mx-auto px-6 flex flex-col justify-center text-center z-10 md:text-left flex-wrap max-w-s md:max-w-3xl my-16 md:mb-36 md:mt-40">
-                <div className="text-2xl md:text-5xl md:leading-snug tracking-tight">
+              <div className="container max-w-screen-md mx-auto px-6 flex flex-col justify-center text-center z-10 md:text-left flex-wrap max-w-s md:max-w-3xl my-16 md:mb-36 md:mt-40">                <div className="text-2xl md:text-5xl md:leading-snug tracking-tight">
                   <b className="text-transparent bg-clip-text bg-gradient-to-tr from-rose-400 via-fuchsia-500 to-indigo-500">UX Designer</b>
                   &nbsp;focused on delivering simple, yet effective, user-centric designs.<br />
                 </div>
