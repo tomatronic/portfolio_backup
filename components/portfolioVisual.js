bfad169de07c4d6bf8155b7f9b44c227d089991e
Tom Spencer <tom.m.spencer@gmail.com>
2024-03-01 17:39:43 +0000

    Trying to align things

--- components/portfolioVisual.js
+++ components/portfolioVisual.js
@@ -38,7 +38,7 @@ const DotMatrix = ({ rows, columns, dotSize, gapSize }) => {
     for (let row = 0; row < rows; row++) {
       for (let col = 0; col < columns; col++) {
         const dotX = col * (dotSize * 2 + gapSize) + dotSize + gapSize;
-        const dotY = row * (dotSize * 2 + gapSize) + dotSize + gapSize;
+        const dotY = row * (dotSize * 2 + gapSize) + dotSize + gapSize - containerY;
         const containerX = containerRect.left;
         const containerY = containerRect.top + window.scrollY; // Update this line
   
