d4d1e66e156f7cfe784a149aecb67cb11b87d648
Tom Spencer <tom.m.spencer@gmail.com>
2024-03-03 16:01:57 +0000

    Remove interaction

--- components/portfolioVisual.js
+++ components/portfolioVisual.js
@@ -1,9 +1,7 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react';
 
 const DotMatrix = ({ rows, columns, dotSize, gapSize }) => {
   const [dots, setDots] = useState([]);
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  const containerRef = useRef(null);
 
   useEffect(() => {
     const initialDots = [];
@@ -14,119 +12,9 @@ const DotMatrix = ({ rows, columns, dotSize, gapSize }) => {
       }
     }
     setDots(initialDots);
-
   }, [rows, columns]);
 
-  const calculateDistance = (dotX, dotY, mouseX, mouseY) => {
-    const deltaX = dotX - mouseX;
-    const deltaY = dotY - mouseY;
-    return Math.sqrt(deltaX * deltaX + deltaY * deltaY) - dotSize;
-  };
-
-  const handleMouseMove = (event) => {
-    setMousePosition({ x: event.pageX, y: event.pageY });
-  };
-
-  const createDots = (mouseX, mouseY) => {
-    const container = containerRef.current;
-    const containerRect = container ? container.getBoundingClientRect() : { left: 0, top: 0 };
-    const containerX = containerRect.left;
-    const containerY = containerRect.top + (typeof window !== 'undefined' ? window.scrollY : 0);
-  
-    const newDots = [];
-  
-    for (let row = 0; row < rows; row++) {
-      for (let col = 0; col < columns; col++) {
-        const dotX = col * (dotSize * 2 + gapSize) + dotSize + gapSize;
-        const dotY = row * (dotSize * 2 + gapSize) + dotSize + gapSize;
-  
-        const distance = calculateDistance(
-          dotX + containerX,
-          dotY + containerY,
-          mouseX,
-          mouseY
-        );
-        const isInfluenceRadius = distance < 24;
-        const baseOpacity = dots[row * columns + col];
-        const opacity = isInfluenceRadius ? 0.4 : baseOpacity;
-  
-        newDots.push(
-          <circle
-            key={`${row}-${col}`}
-            cx={dotX + containerX}
-            cy={dotY + containerY}
-            r={dotSize}
-            fill="#273959"
-            style={{
-              opacity,
-              transition: 'opacity 0.1s ease-in 0.5 ease-out',
-              filter: isInfluenceRadius ? `drop-shadow(5px 5px 5px red)` : 'none',
-            }}
-          />
-        );
-      }
-    }
-    return newDots;
-  };
-  
-  
-
-  return (
-    <div
-      id="dot-container"
-      style={{
-        position: 'absolute',
-        width: '100%',
-        maxWidth: '690px',
-        height: '200%',
-        top: '-120px',
-        left: '0',
-        right: '0',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        WebkitMaskImage: 'radial-gradient(circle at center, white 0, transparent 45%)'
-      }}
-      onMouseMove={handleMouseMove}
-    >
-      <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" style={{ pointerEvents: 'none' }}>
-        {createDots(mousePosition.x, mousePosition.y)}
-      </svg>
-    </div>
-  );
-};
-
-export default DotMatrix;
-
-
-/* import React, { useState, useEffect } from 'react';
-
-const DotMatrix = ({ rows, columns, dotSize, gapSize }) => {
-  const [dots, setDots] = useState([]);
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const initialDots = [];
-    for (let row = 0; row < rows; row++) {
-      for (let col = 0; col < columns; col++) {
-        const baseOpacity = Math.random() * 0.4;
-        initialDots.push(baseOpacity);
-      }
-    }
-    setDots(initialDots);
-
-  }, [rows, columns]);
-
-  const calculateDistance = (dotX, dotY, mouseX, mouseY) => {
-    const deltaX = dotX - mouseX;
-    const deltaY = dotY - mouseY;
-    return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-  };
-
-  const handleMouseMove = (event) => {
-    setMousePosition({ x: event.pageX, y: event.pageY });
-  };
-
-  const createDots = (mouseX, mouseY) => {
+  const createDots = () => {
     const newDots = [];
 
     for (let row = 0; row < rows; row++) {
@@ -134,15 +22,7 @@ const DotMatrix = ({ rows, columns, dotSize, gapSize }) => {
         const dotX = col * (dotSize * 2 + gapSize) + dotSize + gapSize;
         const dotY = row * (dotSize * 2 + gapSize) + dotSize + gapSize;
 
-        const distance = calculateDistance(
-          dotX,
-          dotY,
-          mouseX,
-          mouseY
-        );
-        const isInfluenceRadius = distance < 24;
         const baseOpacity = dots[row * columns + col];
-        const opacity = isInfluenceRadius ? 0.4 : baseOpacity;
 
         newDots.push(
           <circle
@@ -152,9 +32,8 @@ const DotMatrix = ({ rows, columns, dotSize, gapSize }) => {
             r={dotSize}
             fill="#273959"
             style={{
-              opacity,
+              opacity: baseOpacity,
               transition: 'opacity 0.1s ease-in 0.5 ease-out',
-              filter: isInfluenceRadius ? `drop-shadow(5px 5px 5px red)` : 'none',
             }}
           />
         );
@@ -162,7 +41,6 @@ const DotMatrix = ({ rows, columns, dotSize, gapSize }) => {
     }
     return newDots;
   };
-
 
   return (
     <div
@@ -177,16 +55,14 @@ const DotMatrix = ({ rows, columns, dotSize, gapSize }) => {
         right: '0',
         marginLeft: 'auto',
         marginRight: 'auto',
-        WebkitMaskImage: 'radial-gradient(circle at center, white 0, transparent 45%)'
+        WebkitMaskImage: 'radial-gradient(circle at center, white 0, transparent 45%)',
       }}
-      onMouseMove={handleMouseMove}
     >
       <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" style={{ pointerEvents: 'none' }}>
-        {createDots(mousePosition.x, mousePosition.y)}
+        {createDots()}
       </svg>
     </div>
   );
 };
 
 export default DotMatrix;
- */
