5d3f55035583ba12655871f9f8e2b57a4979204b
Tom Spencer <tom.m.spencer@gmail.com>
2024-03-03 09:54:23 +0000

    Dot reduction

--- components/portfolioVisual.js
+++ components/portfolioVisual.js
@@ -43,7 +43,7 @@ const DotMatrix = ({ rows, columns, dotSize, gapSize }) => {
         const dotY = row * (dotSize * 2 + gapSize) + dotSize + gapSize;
 
         const distance = calculateDistance(
-          dotX + containerPosition.left + 12,
+          dotX + containerPosition.left,
           dotY + containerPosition.top + 12,
           mouseX,
           mouseY
